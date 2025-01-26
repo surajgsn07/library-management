446b3189d78d1cbce71a50821cc7c464c09fe23b
surajgsn07 <surajgn07@gmail.com>
2025-01-27 00:05:25 +0530

    m

--- frontend/src/components/admin/dashboard/ManageUsers.jsx
+++ frontend/src/components/admin/dashboard/ManageUsers.jsx
@@ -125,14 +125,28 @@ const ManageUsers = () => {
           placeholder="Name"
           className="p-3 border border-gray-300 rounded-md"
         />
-        <input
-          type="text"
-          name="course"
-          value={formData.course}
-          onChange={handleChange}
-          placeholder="Course"
-          className="p-3 border border-gray-300 rounded-md"
-        />
+        
+        <select
+                id="class"
+                name="class"
+                value={formData.course}
+                onChange={handleChange}
+                className="w-full mt-1 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:border-transparent"
+                placeholder="Course"
+              >
+                <option value="">Select course</option>
+                <option value="B.Tech">B.Tech</option>
+                <option value="M.Tech">M.Tech</option>
+                <option value="MCA">MCA</option>
+                
+                <option value="B.Tech">BCA</option>
+                <option value="M.Tech">B.Pharmacy</option>
+                <option value="MCA">BBA</option>
+                
+                <option value="B.Tech">B.Com</option>
+                <option value="M.Tech">FD</option>
+                <option value="MCA">BAJMC</option>
+              </select>
         <button
           type="submit"
           className="bg-blue-600 text-white p-3 rounded-md col-span-1 sm:col-span-3 lg:col-span-1"
